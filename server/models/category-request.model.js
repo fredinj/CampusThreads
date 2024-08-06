906270b62ceb983a56d281836030430b8886615b
rose-michelle-mathew <michellemathew024@gmail.com>
2024-08-06 06:41:28 +0530

    michelle commit

--- server/models/category-request.model.js
+++ server/models/category-request.model.js
@@ -4,9 +4,10 @@ const categoryRequestSchema = new mongoose.Schema({
     categoryName: { type: String, required: true },
     description: { type: String, required: true },
     requestedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    status: { type: String, enum: ['pending', 'approved', 'rejected'], default: 'pending' }
+    status: { type: String, enum: ['pending', 'approved', 'rejected'], default: 'pending' },
+    tags: [{ type: String }] // Array of strings for tags
 });
 
-const CategoryRequest = mongoose.model('category-request', categoryRequestSchema);
+const CategoryRequest = mongoose.model('CategoryRequest', categoryRequestSchema);
 
 module.exports = CategoryRequest;
