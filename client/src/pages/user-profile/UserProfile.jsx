0aab402d29cf1b966333e9afebf18175379c3dab
Fredin <90355410+fredinj@users.noreply.github.com>
2024-08-20 08:34:04 +0530

    merge all branches

--- client/src/pages/user-profile/UserProfile.jsx
+++ client/src/pages/user-profile/UserProfile.jsx
@@ -1,12 +1,13 @@
 // src/pages/UserProfile.jsx
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../contexts/AuthContext';
+import { useNavigate } from 'react-router-dom';
 import { FaPencilAlt } from 'react-icons/fa';
 import axios from 'axios';
 import './UserProfile.css';
 
 const UserProfile = () => {
-  const { isAuthenticated, user } = useContext(AuthContext);
+  const { isAuthenticated, user, logout } = useContext(AuthContext);
   const [isEditing, setIsEditing] = useState({
     firstName: false,
     lastName: false,
@@ -16,9 +17,17 @@ const UserProfile = () => {
   const [editableUser, setEditableUser] = useState(user);
   const [hasChanges, setHasChanges] = useState(false);
 
-  useEffect(() => {
-    setEditableUser(user);
-  }, [user]);
+  const navigate = useNavigate()
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+      navigate("/");
+    } catch (error) {
+      console.error("Logout failed", error);
+      setError("Logout failed. Please try again.");
+    }
+  };
 
   const handleEditToggle = (field) => {
     setIsEditing({ ...isEditing, [field]: !isEditing[field] });
@@ -63,6 +72,10 @@ const UserProfile = () => {
     }
   };
 
+  useEffect(() => {
+    setEditableUser(user);
+  }, [user]);
+
   if (!isAuthenticated) {
     return <div>Please log in to see your profile.</div>;
   }
@@ -72,74 +85,100 @@ const UserProfile = () => {
   }
 
   return (
-    <div className="user-profile-container">
-      <h1 className="user-profile-heading"><b>User Profile</b></h1>
-      <div className="user-profile-details">
-        <div className="user-profile-detail">
-          <div className="user-profile-field">
-            <label className="user-profile-detail-label">First Name:</label>
-            {isEditing.firstName ? (
-              <input
-                type="text"
-                name="firstName"
-                value={editableUser.firstName}
-                onChange={handleInputChange}
-                className="user-profile-input"
-              />
-            ) : (
-              <span>{editableUser.firstName}</span>
-            )}
+    <nav className='flex flex-col items-center justify-center'>
+      <nav className="mt-4">
+      <button
+        className="ml-2 rounded border border-black px-2 py-1"
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        Home
+      </button>
+      <button
+        className="ml-2 rounded border border-black px-2 py-1"
+        onClick={() => {
+          navigate(`/categories`);
+        }}
+      >
+        Categories
+      </button>
+      <button
+        className="ml-2 rounded border border-black px-2 py-1"
+        onClick={handleLogout}
+      >
+        Logout
+      </button>
+    </nav>
+      <div className="user-profile-container">
+        <h1 className="user-profile-heading"><b>User Profile</b></h1>
+        <div className="user-profile-details">
+          <div className="user-profile-detail">
+            <div className="user-profile-field">
+              <label className="user-profile-detail-label">First Name:</label>
+              {isEditing.firstName ? (
+                <input
+                  type="text"
+                  name="firstName"
+                  value={editableUser.firstName}
+                  onChange={handleInputChange}
+                  className="user-profile-input"
+                />
+              ) : (
+                <span>{editableUser.firstName}</span>
+              )}
+            </div>
+            <FaPencilAlt
+              className="user-profile-edit-icon"
+              onClick={() => handleEditToggle('firstName')}
+            />
           </div>
-          <FaPencilAlt
-            className="user-profile-edit-icon"
-            onClick={() => handleEditToggle('firstName')}
-          />
-        </div>
-        <div className="user-profile-detail">
-          <div className="user-profile-field">
-            <label className="user-profile-detail-label">Last Name:</label>
-            {isEditing.lastName ? (
-              <input
-                type="text"
-                name="lastName"
-                value={editableUser.lastName}
-                onChange={handleInputChange}
-                className="user-profile-input"
-              />
-            ) : (
-              <span>{editableUser.lastName}</span>
-            )}
+          <div className="user-profile-detail">
+            <div className="user-profile-field">
+              <label className="user-profile-detail-label">Last Name:</label>
+              {isEditing.lastName ? (
+                <input
+                  type="text"
+                  name="lastName"
+                  value={editableUser.lastName}
+                  onChange={handleInputChange}
+                  className="user-profile-input"
+                />
+              ) : (
+                <span>{editableUser.lastName}</span>
+              )}
+            </div>
+            <FaPencilAlt
+              className="user-profile-edit-icon"
+              onClick={() => handleEditToggle('lastName')}
+            />
           </div>
-          <FaPencilAlt
-            className="user-profile-edit-icon"
-            onClick={() => handleEditToggle('lastName')}
-          />
-        </div>
-        <div className="user-profile-detail">
-          <div className="user-profile-field">
-            <label className="user-profile-detail-label">Email:</label>
-            {isEditing.email ? (
-              <input
-                type="email"
-                name="email"
-                value={editableUser.email}
-                onChange={handleInputChange}
-                className="user-profile-input"
-              />
-            ) : (
-              <span>{editableUser.email}</span>
-            )}
+          <div className="user-profile-detail">
+            <div className="user-profile-field">
+              <label className="user-profile-detail-label">Email:</label>
+              {isEditing.email ? (
+                <input
+                  type="email"
+                  name="email"
+                  value={editableUser.email}
+                  onChange={handleInputChange}
+                  className="user-profile-input"
+                />
+              ) : (
+                <span>{editableUser.email}</span>
+              )}
+            </div>
+            <FaPencilAlt
+              className="user-profile-edit-icon"
+              onClick={() => handleEditToggle('email')}
+            />
           </div>
-          <FaPencilAlt
-            className="user-profile-edit-icon"
-            onClick={() => handleEditToggle('email')}
-          />
+          <button className="user-profile-save-button" onClick={handleSave}>
+            Save
+          </button>
         </div>
-        <button className="user-profile-save-button" onClick={handleSave}>
-          Save
-        </button>
       </div>
-    </div>
+      </nav>
   );
 };
 
